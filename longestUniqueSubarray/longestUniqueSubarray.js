b0e923359a3f7e4e4681fb6f5f543ca2b8bd76ab
jamieberrier <berrier.jamie@gmail.com>
2021-03-17 17:31:52 -0700

    Update longestUniqueSubarray.js

--- longestUniqueSubarray/longestUniqueSubarray.js
+++ longestUniqueSubarray/longestUniqueSubarray.js
@@ -5,8 +5,16 @@ Given an array of elements, return the length of the longest subarray where all 
 For example, given the array [5, 1, 3, 5, 2, 3, 4, 1], return 5, as the longest subarray of distinct elements is [5, 2, 3, 4, 1].
 */
 
+const assert = require('assert')
+
 const longestUniqueSubarray = arr => {
 
 };
 
 console.log(longestUniqueSubarray([5, 1, 3, 5, 2, 3, 4, 1])); // 5
+// shouldn't do anything
+assert.strictEqual(1, 2)
+// should break
+assert.strictEqual(1, 2)
+//
+assert.deepStrictEqual(ourFunction([5, 1, 3, 5, 2, 3, 4, 1], 5))
