f926c91a7413f20eb184e4630340bd60bcda9ec6
jamieberrier <berrier.jamie@gmail.com>
2021-03-17 17:26:59 -0700

    Update longestUniqueSubarray.js

--- longestUniqueSubarray/longestUniqueSubarray.js
+++ longestUniqueSubarray/longestUniqueSubarray.js
@@ -4,3 +4,9 @@ Given an array of elements, return the length of the longest subarray where all 
 
 For example, given the array [5, 1, 3, 5, 2, 3, 4, 1], return 5, as the longest subarray of distinct elements is [5, 2, 3, 4, 1].
 */
+
+const longestUniqueSubarray = arr => {
+
+};
+
+console.log(longestUniqueSubarray([5, 1, 3, 5, 2, 3, 4, 1])); // 5
