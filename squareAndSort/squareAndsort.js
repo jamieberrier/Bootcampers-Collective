247228b06e5098863395868f2ea4568b17249062
jamieberrier <berrier.jamie@gmail.com>
2021-03-17 19:09:05 -0700

    Update squareAndsort.js

--- squareAndSort/squareAndsort.js
+++ squareAndSort/squareAndsort.js
@@ -5,3 +5,19 @@ and give the output in sorted order.
 
 For example, given [-9, -2, 0, 2, 3], return [0, 4, 4, 9, 81].
 */
+
+const squareAndSort = arr => {
+  let squares = arr.map(num => num ** 2)
+
+  for(let i = 0; i < squares.length; i++) {
+    if(squares[i] > squares[i + 1]) {
+      let temp = squares[i]
+      squares[i] = squares[i + 1]
+      squares[i + 1] = temp
+    }
+  }
+
+  return squares
+}
+
+squareAndSort([-2,0,2,4,8])
